e61e2d4776c09c15eb3f3e3e7c2b58f261147209
Ponneelan <ponneelan1305@gmail.com>
2023-05-05 19:01:16 +0530

    express appliaction

---
+++ Express-HelloWorld/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+
+app.get('/',(req,res)=>{
+    console.log(req);
+    console.log('i am inside get');
+    // res.send('Hi welcome Name is  ' + req.query['name'] + ' age is '+req.query['age']);
+    res.json([
+        {name:req.query['name'],age:req.query['age']},
+        {name:req.query['name'],age:req.query['age']},
+        {name:req.query['name'],age:req.query['age']},
+    ]);
+    console.log(req.query['name'] )
+})
+
+let usersList = [
+    {name:'Ponneelan',age:22},
+    {name:'Thangam',age:20},
+    {name:'Vicky',age:22},
+    {name:'Ragul',age:22}
+];
+app.get('/users',(req,res)=>{
+    res.json(usersList);
+})
+
+app.get('/getuser/:age',(req,res)=>{
+    res.json(usersList.filter(e => e.age == req.params['age']));
+});
+
+app.get('/myapi',()=>{
+    console.log('i am inside myapi');
+})
+
+app.listen(port,()=>{
+    console.log('listening on port 3000');
+})
+
+
+
